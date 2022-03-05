7c9b4f8f96e277737309e84e645e644bcea92c20
FR-User <freeofrregist005@gmail.com>
2022-03-05 08:16:15 +0000

    hello

--- src/asset/script/events/onHit.js
+++ src/asset/script/events/onHit.js
@@ -1,10 +1,12 @@
 var onHit = {
   playerVsSlime : function(player, target){
     //console.log(target.destroy());
+    //http://54.180.90.186:56662/
   },
   playerVsEnemy : function(player, target){
     console.log(target.damage);
     gameScene.this.physics.pause();
     player.setTint(0xff0000);
+    gameScene.this.scene.stop();
   },
 }
